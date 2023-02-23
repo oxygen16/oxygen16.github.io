d57656b9e3ff68fbe4c59cd648cb2253ff787181
万春明 <oxygen16@users.noreply.github.com>
2023-02-23 14:30:58 +0800

    upload

---
+++ js/swit.js
@@ -0,0 +1,17 @@
+// JavaScript Document
+<!--
+		var scrollPic_02 = new ScrollPic();
+					scrollPic_02.scrollContId   = "ISL_Cont_1"; //��������ID
+					scrollPic_02.arrLeftId      = "Leftarr";//���ͷID
+					scrollPic_02.arrRightId     = "Rightarr"; //�Ҽ�ͷID
+			
+					scrollPic_02.frameWidth     = 1150;//��ʾ�����
+					scrollPic_02.pageWidth      = 1; //��ҳ����
+			
+					scrollPic_02.speed          = 20; //�ƶ��ٶ�(��λ���룬ԽСԽ��)
+					scrollPic_02.space          = 20; //ÿ���ƶ�����(��λpx��Խ��Խ��)
+					scrollPic_02.autoPlay       = true; //�Զ�����
+					scrollPic_02.autoPlayTime   = 0.018; //�Զ����ż��ʱ��(��)
+			
+					scrollPic_02.initialize(); //��ʼ��
+//-->
